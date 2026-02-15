c9bbcfc3c0a404fd214696c2762b2936a2f7dc0a
Sathyan S S <sssathyan2004@gmail.com>
2026-02-15 16:27:44 +0530

    Create 07_Scoreboard.sv

---
+++ 01. Half Adder/07_Scoreboard.sv
@@ -0,0 +1,21 @@
+class scoreboard;
+  transaction trans;
+  mailbox montoscore;
+  
+  function new(mailbox montoscore);
+    this.montoscore=montoscore;
+  endfunction
+  
+  task main();
+    repeat(6) begin
+      montoscore.get(trans);
+      trans.display("Scoreboard");
+      
+      if (((trans.a^trans.b)==trans.sum)&&((trans.a&trans.b)==trans.carry)))
+        $display("Verification Success");
+      else
+        $display("Verification Failure");
+    end
+  endtask
+  
+endclass
