30abbbceb21d039a7418f1d737d65392aa3b919d
Sathyan S S <sssathyan2004@gmail.com>
2026-02-23 19:22:48 +0530

    Add driver class for Full Adder simulation

---
+++ 02. Full Adder/05_Driver.sv
@@ -0,0 +1,21 @@
+class driver;
+  virtual variable intf;
+  mailbox gentodri;
+  
+  function new(virtual variable intf, mailbox gentodri);
+    this.intf=intf;
+    this.gentodri=gentodri;
+  endfunction
+  
+  task main();
+    transaction tr;
+    repeat(8) begin
+      gentodri.get(tr);
+      intf.a<=tr.a;
+      intf.b<=tr.b;
+      intf.c<=tr.c;
+      #1;
+    end
+  endtask
+  
+endclass
